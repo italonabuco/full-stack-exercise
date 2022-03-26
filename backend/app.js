26ba136b3ce01fd11aaa866216638591bd3e2db4
Italo Nabuco <italonabuco@hotmail.com>
2022-03-26 20:47:54 -0300

    improve endpoints - convert req.body with body-parser - improve GET /team - create POSR /team

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,36 @@
-const express = require('express');
-const { TeamMember } = require('./model');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { TeamMember } = require("./model");
 
 const app = express();
 
-app.get('/team', async (req, res, next) => {
-    const team = await TeamMember.findAll();
+app.use(bodyParser.json());
 
-    // Please filter out createdAt and updatedAt properties
-    return res.json(team);
+app.get("/team", async (req, res, next) => {
+  const team = await TeamMember.findAll({
+    // 1 - filter out createdAt and updatedAt properties
+    attributes: { exclude: ["createdAt", "updatedAt"] },
+    // 2 - order by firstName ASC
+    order: [["firstName"]],
+  });
+
+  return res.json(team);
 });
 
-// Please create an endpoint for adding team members here
+// 3 - Please create an endpoint for adding team members here
+app.post("/team", async (req, res) => {
+  try {
+    // require: firstName, lastName
+    const { firstName, lastName } = req.body;
+    const newTeamMember = await TeamMember.create({
+      firstName,
+      lastName,
+    });
+    return res.json({ ...newTeamMember.dataValues });
+  } catch (e) {
+    console.log(e);
+    return res.json({ error: e.message, message: "An error occured!" });
+  }
+});
 
 module.exports = app;
